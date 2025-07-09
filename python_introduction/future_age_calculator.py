047c339923da51322cc1105a00b319310662a6eb
Shaul Ratemo <ratemoshaul@gmail.com>
2025-07-09 16:21:47 +0300

    Add future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+age = int(input("How old are you? "))
+current_year = 2025
+future_year = 2050
+year_difference = future_year - current_year
+projected_age = age + year_difference
+
+print("In ", future_year, ", you will be ", projected_age, " years old.")
