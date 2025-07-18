7e31065c79f9d54aebf89613d25ec0e322018220
Shaul Ratemo <ratemoshaul@gmail.com>
2025-07-18 15:43:43 +0300

    Fix bugs

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,18 +6,18 @@ if task and priority and time_bound:
     match priority:
         case "high":
             if time_bound == "yes":
-                print(f"'{task}' is a high priority task that requires immediate attention today!")
+                print(f"Reminder: '{task}' is a high priority task that requires immediate attention today!")
             elif time_bound == "no":
-                print(f"'{task}' is a high priority task. Consider completing it when you have free time.")
+                print(f"Reminder: '{task}' is a high priority task. Consider completing it when you have free time.")
         case "medium":
             if time_bound == "yes":
-                print(f"'{task}' is a medium priority task that requires immediate attention today!")
+                print(f"Reminder: '{task}' is a medium priority task that requires immediate attention today!")
             elif time_bound == "no":
-                print(f"'{task}' is a medium priority task. Consider completing it when you have free time.")
+                print(f"Reminder: '{task}' is a medium priority task. Consider completing it when you have free time.")
         case "low":
             if time_bound == "yes":
-                print(f"'{task}' is a low priority task that requires immediate attention today!")
+                print(f"Reminder: '{task}' is a low priority task that requires immediate attention today!")
             elif time_bound == "no":
-                print(f"'{task}' is a low priority task. Consider completing it when you have free time.")
+                print(f"Reminder: '{task}' is a low priority task. Consider completing it when you have free time.")
 else:
     print("Some fields are missing inputs. Please try again later!")
