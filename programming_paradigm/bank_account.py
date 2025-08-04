3d02eca524cd60b6256538882aff2e53e8a22c86
Shaul Ratemo <ratemoshaul@gmail.com>
2025-08-04 12:39:42 +0300

    Add main-0.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,7 +10,8 @@ class BankAccount:
         if 0 < amount <= self.__account_balance:
             self.__account_balance -= amount
             return True
-        return False
+        else:
+            return False
     
     def display_balance(self):
         print(f"Current Balance: ${self.__account_balance}")
