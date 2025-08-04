fb379d9117c8886d6e4ce0e78b90809ec9e83cf9
Shaul Ratemo <ratemoshaul@gmail.com>
2025-08-04 11:47:48 +0300

    Create simple banking system

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+class BankAccount:
+    def __init__(self, account_balance=0):
+        self.__account_balance = account_balance
+    
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited: {amount}")
+    
+    def withdraw(self, amount):
+        if amount > self.__account_balance:
+            print("Insufficient funds.")
+        else:
+            self.__account_balance -= amount
+            print(f"Withdrew: {amount}")
+    
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance}")
+    
+    
+    
