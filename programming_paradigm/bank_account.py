6c9734d58e6a7e05a36e4138e59473e5e1e26ce2
Shaul Ratemo <ratemoshaul@gmail.com>
2025-08-04 12:27:03 +0300

    Bug fix on withdraw

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,16 +1,16 @@
 class BankAccount:
     def __init__(self, account_balance=0):
-        self.account_balance = account_balance
+        self.__account_balance = account_balance
     
     def deposit(self, amount):
         if amount > 0:
-            self.account_balance += amount
+            self.__account_balance += amount
     
     def withdraw(self, amount):
-        if amount <= self.account_balance:
-            self.account_balance -= amount
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
             return True
         return False
     
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.__account_balance}")
